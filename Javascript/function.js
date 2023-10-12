bd8aa524e7af06639cb7ab42bd93554986032a25
Ansu <aryalansu2064@gmail.com>
2023-10-12 08:08:11 +0545

    html done

---
+++ Javascript/function.js
@@ -0,0 +1,68 @@
+/*
+Function 
+=> 
+
+
+
+
+//function call 
+function_name()
+
+*/
+// var name; // variable declaration 
+// name = 'ram' //variable initialization 
+// function sum(){
+//     var a = 23; 
+//     var b = 234; 
+//     var sum1 = a + b;
+//     console.log(sum)
+// }
+// sum()
+// sum()
+// sum()
+// console.log('hi')
+
+// function div(){
+//     var a = 2
+//     var b = 100 
+//     var div = b / a 
+//     console.log(div)
+// } 
+// div()
+// div()
+
+
+// function multi(){
+//     var a = 65
+//     var b = 89 
+//     var multi = b * a
+//     console.log(multi)
+// }
+// multi()
+
+// function add(){
+//     var a = 500
+//     var b = 450
+//     var c = 700 
+//     var d =65
+//     var add = a + b + c + d 
+//     console.log(add)
+// }
+// add()
+
+function sub(){
+    var a = 10000 
+    var b = 500 
+    var c = 300 
+    var sub = a - b - c 
+    console.log(sub)
+}
+sub()
+
+function remin(){
+    var a = 900
+    var b = 30 
+    var remin = 900/30 
+    console.log(remin)
+}
+remin()
